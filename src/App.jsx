96775de7bcf209efde0947a80524cdadbf1a8cec
lucasilliano <lucas.illiano@hotmail.com>
2024-09-25 14:41:21 +0200

    cleanning for a good start

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
 
   return (
       <>
-          <h1 className="text-3xl font-bold underline">
-              Hello world!
+          <h1 className="text-3xl font-bold">
+              React Weather App
           </h1>
       </>
   )
